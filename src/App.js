0a3b22287694cedb2996256f4d70ef7e7533fa4f
zisilva <zimarlensilva@gmail.com>
2023-02-04 11:25:25 -0800

    creating responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar  from './components/navbar'
+import Navbar from "./components/Navbar/Navbar";
 import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
- return <div className="App">
-  <ShopContextProvider>
-    <Router>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Shop />}></Route>
-        <Route path="/contact" element={<Contact />} />
-        <Route path="cart" element = {<Cart />}></Route>
-      </Routes>
-    </Router>
-    </ShopContextProvider> 
-   </div>
+  return (
+    <div className="App">
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        redirectUri={window.location.origin}
+      >
+        <ShopContextProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Shop />}></Route>
+              <Route path="/contact" element={<Contact />} />
+              <Route path="cart" element={<Cart />}></Route>
+            </Routes>
+          </Router>
+        </ShopContextProvider>
+      </Auth0Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
+
+  return (
+    <>
+  <nav className="navbar">
+      <div className="navbar-main">
+          <Link to='/' className='navbar-box' onClick={closeMobileMenu}>
+            <h1 className='navbar-logo' > Minimalist Shop </h1> 
+            </Link>
+            </div>
+            <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fa-solid fa-x" : "fas fa-bars"} />
+          </div>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-list" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/comments"
+                className="nav-list"
+                onClick={closeMobileMenu}>
+                Comments
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="country-list"
+                className="nav-list"
+                onClick={closeMobileMenu}>
+                Country List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to='/sign-up'
+                className='nav-list-btn'
+                onClick={closeMobileMenu}>
+                {button && <button className='btn-nav'  >SIGN UP</button>}
+              </Link>
+            </li>
+          </ul>
+  </nav>
+    </>
+  );
+}
+
+export default Navbar;
